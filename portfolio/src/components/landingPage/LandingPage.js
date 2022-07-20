0a74550b9d4e5dd329bb81b869b26aea1076603e
alexmercer <alex.mercer6@hotmail.com>
2022-07-20 21:52:43 +1000

    scrolling title parallex

--- portfolio/src/components/landingPage/LandingPage.js
+++ portfolio/src/components/landingPage/LandingPage.js
@@ -1,10 +1,19 @@
 import "./LandingPage.css"
+import { Parallax, ParallaxProvider } from "react-scroll-parallax"
+
 function LandingPage() {
     return (
         <div className="landing-page">
             <div className="title-container">
-                <h1>Alex Mercer.</h1>
-                <h3>Full Stack Developer</h3>
+                <ParallaxProvider>
+                    <Parallax translateX={[170, -200]} opacity={[3, -1]}>
+                        <h1 className="title">Alex Mercer.</h1>
+                    </Parallax>
+
+                    <Parallax translateX={[0, 0]} opacity={[2, -0.9]}>
+                        <h3>Full Stack Developer</h3>
+                    </Parallax>
+                </ParallaxProvider>
             </div>
         </div>
     )
