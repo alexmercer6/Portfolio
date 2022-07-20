179c83cd03288643cd9bf594f6b88e0eba9fc39b
alexmercer <alex.mercer6@hotmail.com>
2022-07-20 20:33:27 +1000

    title on screen

--- portfolio/src/components/landingPage/LandingPage.js
+++ portfolio/src/components/landingPage/LandingPage.js
@@ -1,6 +1,13 @@
 import "./LandingPage.css"
 function LandingPage() {
-    return <div className="landing-page"></div>
+    return (
+        <div className="landing-page">
+            <div className="title-container">
+                <h1>Alex Mercer.</h1>
+                <h3>Full Stack Developer</h3>
+            </div>
+        </div>
+    )
 }
 
 export default LandingPage
